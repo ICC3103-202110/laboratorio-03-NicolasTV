ed801eaa1bdf147c1082d9e30701325433e93cad
Nicolás Torres Vargas <natorres@miuandes.cl>
2021-04-19 18:52:54 -0400

    start game y players

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+from player import Player
+
+players = []
+
+
+def start_game():
+
+    print("\n·················· Welcome to Coup ··················")
+    print("This board game is played with a minimum of 3 players and a maximum of 4.\n")
+    n_players = int(input("Enter the number of players:  "))
+    while n_players < 3 or n_players > 4:
+        print("Invalid number of players, try again\n")
+        n_players = int(input("Enter the number of players:  "))
+
+    for i in range(n_players):
+        user_id = i + 1
+        name = str(input(f"\nWrite the name of player {user_id}: "))
+        players.append(Player(name, user_id))
+
+
+def console():
+
+    while true:
+        start_game()
+
 
 if __name__ == "__main__":
-    main()
-
-
-def main():
-    print("")
+    print(start_game())
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 
 class Player():
 
-    def __init(self, ):
-        
+    def __init__(self, name, user_id):
+        self.__name = name
+        self.__user_id = user_id
